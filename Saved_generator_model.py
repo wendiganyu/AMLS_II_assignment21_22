e24b9a598f57b0337971637f639bba55326c1be0
wendiganyu <jinyang2@andrew.cmu.edu>
2022-04-15 01:30:15 +0100

    testing loading saved model to output the desired image

--- Saved_generator_model.py
+++ Saved_generator_model.py
@@ -29,7 +29,8 @@ def load_saved_generator(PATH, upscale_factor):
 
 
 if __name__ == '__main__':
-    model_G = load_saved_generator("results/gen_bestPSNR_seed13414773155953270327.pth.tar", upscale_factor=2)
+    # model_G = load_saved_generator("results/gen_bestPSNR_seed13414773155953270327.pth.tar", upscale_factor=2)
+    model_G = load_saved_generator("results/gen_bestPSNR_seed10095113155149744729.pth.tar", upscale_factor=2) #SRResnet
 
     upscale_factor = 2
     SR_width = 180
@@ -41,12 +42,20 @@ if __name__ == '__main__':
     LR_img_path = "Datasets/test/LR_bicubic_X2/0886x2.png"
     input_LR_img = img = cv2.imread(LR_img_path).astype(np.float32) / 255
     # top_coord: 400, left_coord:510
-    input_LR_crop = Utils.img_custom_crop(input_LR_img, SR_height // upscale_factor, SR_width // upscale_factor, 400,
-                                          510)
+    input_LR_crop = Utils.img_custom_crop(input_LR_img, SR_height // upscale_factor, SR_width // upscale_factor, 150,
+                                          300)
+
+    input_LR_crop = cv2.cvtColor(input_LR_crop, cv2.COLOR_BGR2RGB)
+    input_LR_crop = input_LR_crop.transpose(2,0,1)
 
     input_LR_crop = torch.Tensor(input_LR_crop)
+    input_LR_crop = input_LR_crop.unsqueeze(0)
 
     SR_img = model_G(input_LR_crop)
-    SR_img = np.array(SR_img)
+
+    SR_img = SR_img.detach().numpy()
     SR_img = SR_img.astype(np.float32) * 255
-    cv2.imwrite("testing.png", SR_img)
+    SR_img = SR_img.squeeze(0)
+    SR_img = SR_img.transpose(1,2,0)
+    print(SR_img.shape)
+    cv2.imwrite("testing2.png", SR_img)
